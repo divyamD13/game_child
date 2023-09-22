f6f59d3862ad0f8dd8d72a9da421c6cc5b650558
divyamD13 <divyamdivesh13@gmail.com>
2023-09-22 12:05:48 +0530

    splash screen done

--- lib/quizpage.dart
+++ lib/quizpage.dart
@@ -1,1 +1,28 @@
 import 'package:flutter/material.dart';
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.cyan ,
+        title: const Text('Quiz Visualiser',
+          style:TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ) ,
+        ),
+
+
+      ),
+
+
+    );
+  }
+}
--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:game_child/homepage.dart';
+import 'package:game_child/quizpage.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -7,10 +11,33 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+     Timer(Duration(seconds: 5), () {
+
+       Navigator.pushReplacement(context, MaterialPageRoute(builder:(context)=>QuizPage()
+       ));
+     });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: Container(
 
+          color: Colors.cyan,
+          child: Center(
+            child: const Text('GAMZY',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),),
+          ),
+        ),
     );
   }
 }
